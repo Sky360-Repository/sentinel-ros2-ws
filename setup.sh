bf8376f2165ef152b3d472ea30065ca3748632bb
Michael Groenewald <michael.groenewald@gmail.com>
2022-11-11 14:43:24 +0000

    Adding missing package

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ set -e
 
 vcs import < src/ros2.repos src
 sudo apt-get update
+
+sudo apt install -y python3-natsort  # Installing natsort (is not in rosdep)
+
 rosdep update
 rosdep install --from-paths src --ignore-src -y
